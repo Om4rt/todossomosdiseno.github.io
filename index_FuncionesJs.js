551a74a89be5d15e96cbfe01c32879309b3fea71
Om4rt <137016736+Om4rt@users.noreply.github.com>
2025-03-29 13:51:07 -0600

    PrimerCommitTSD
    Primercommit

---
+++ index_FuncionesJs.js
@@ -0,0 +1,8 @@
+
+// Funciones antes de la carga
+document.addEventListener("DOMContentLoaded", function() {
+    
+    // Icono del navbar
+    var iconContainer = document.getElementById("icon-container");
+    iconContainer.innerHTML = '<a class"padding-icon-navbar" href="#"><i class="fas fa-cart-shopping icon-shop"></i></a>';
+});
